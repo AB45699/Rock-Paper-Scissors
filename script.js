2bed0c964dfcee60d025efcc3e7938b729d2f750
AB45699 <b.anam8541@gmail.com>
2025-06-11 21:10:42 +0100

    Implemented function to obtain human choice of RPS

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const choices = ["Rock", "Paper", "Scissors"]
 
 function getComputerChoice() {
     const number = Math.floor(Math.random() * 3);
-    console.log(number)
-    return number
+    const computerChoice = choices[number];
+    console.log(computerChoice)
+    return computerChoice
 };
 
+function getHumanChoice() {
+    let humanChoice = prompt("Pick your choice: Rock, Paper, or Scissors");
+    console.log(humanChoice);
+    return humanChoice
+}
+
+getHumanChoice();
 getComputerChoice()
 
-//hello
+//remember to delete console.logs, clean code rules (func names, var names etc)
